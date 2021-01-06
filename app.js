ba4fd98b6d39d68c5ef3a12f9f3a4b1e081e27e8
Alice Piar <adpir30@gmail.com>
2021-01-06 06:42:12 -0400

    add new style in the code

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
- //const table = require("console.table");
-// console.log(logo(config).render());
-// // console.log(chalkAnimation.rainbow)
-// const gradient = require('gradient-string');
-
-// console.log(gradient('cyan', 'pink')('Hello world!'));
-// console.log(c.yellow(`foo ${c.red.bold('red')} bar ${c.cyan('cyan')} baz`));
+const cTable = require("console.table");
+const gradient = require("gradient-string");
 // const c = require('ansi-colors');
 
 const inquirer = require("inquirer");
@@ -20,7 +15,9 @@ config.textColor = "cyan";
   (config.margin = 3),
   (config.borderColor = "cyan");
 console.log(logo(config).render());
+
 employeesGenerator();
+
 function employeesGenerator() {
   inquirer
     .prompt({
@@ -44,7 +41,6 @@ function employeesGenerator() {
         "Add Departments",
         "Remove Departments",
         "Exit",
-        
       ],
     })
 
@@ -143,6 +139,14 @@ function updateRole() {
           console.log(err);
         }
         console.table(res);
+        console.log(
+          gradient(
+            "cyan",
+            "magenta"
+          )(
+            "*---------------------------------------------------------Update Role --------------------------------------------------------*"
+          )
+        );
         employeesGenerator();
       });
     });
@@ -155,9 +159,16 @@ function updatemanager() {
         type: "list",
         name: "employee_id",
         choices: [
-          { name: "Alex Drews", value: 5 },
-          { name: "Cathia Lopez", value: 4 },
-          { name: "Jay Borden", value: 6 },
+          { name: "Happy Year", value: 8 },
+          { name: "Estrella Shine", value: 9 },
+          { name: "Ana Light", value: 10 },
+          { name: "Julie Saas", value: 11 },
+          { name: "Joshua Lolo", value: 12 },
+          { name: "Dunes Star", value: 13 },
+          { name: "Jasmine Radiance", value: 14 },
+          { name: "Luz Blue", value: 15 },
+          { name: "Jil Kuyh", value: 16 },
+          { name: "Lola Roar", value: 17 },
         ],
         message: "Which employee's you want to update?",
       },
@@ -166,11 +177,13 @@ function updatemanager() {
         type: "list",
         name: "manager_id",
         choices: [
-          { name: "Alex Lopez", value: 19 },
-          { name: "Karl Green", value: 20 },
-          { name: "Jo flower", value: 21 },
-          { name: "Sunny loves", value: 22 },
-          { name: "World lives", value: 23 },
+          { name: "Alexandra Drews", value: 1 },
+          { name: "Carlos Rodriguez", value: 2 },
+          { name: "Carmen Borges", value: 3 },
+          { name: "Cathia Piar", value: 4 },
+          { name: "Sunny Shine", value: 5 },
+          { name: "Jay Borden", value: 6 },
+          { name: "Alice Piar", value: 7 },
         ],
         message: "Choose a new manager?",
       },
@@ -185,12 +198,29 @@ function updatemanager() {
             console.log(err);
           }
           console.table(res);
+          console.log(
+            gradient(
+              "cyan",
+              "magenta"
+            )(
+              "*---------------------------------------------------------Update Manager ---------------------------------------------------*"
+            )
+          );
           employeesGenerator();
         }
       );
     });
 }
 function viewEmplmanager() {
+  console.log(
+    gradient(
+      "cyan",
+      "magenta"
+    )(
+      "*---------------------------------------------------------View Employee Manager ----------------------------------------------------*"
+    )
+  );
+
   const query = `select a.id,a.first_name,a.last_name, concat(b.first_name ," ",b.last_name) "Manager" from employee a, employee b where a.manager_id = b.id;
   `;
   connection.query(query, function (err, data) {
@@ -200,6 +230,15 @@ function viewEmplmanager() {
   });
 }
 function viewEmpldept() {
+  console.log(
+    gradient(
+      "cyan",
+      "magenta"
+    )(
+      "*------------------------------------------------------------View Employee Department-----------------------------------------------*"
+    )
+  );
+
   const query = `SELECT e.id as '#ID', CONCAT_WS(' ', e.first_name,e.last_name) AS Employers,  d.departmentname as 'Departments' from employee e, roles r, department d where
   e.role_id = r.id and r.department_id = d.id;
   `;
@@ -211,8 +250,17 @@ function viewEmpldept() {
 }
 
 function viewDept() {
+  console.log(
+    gradient(
+      "cyan",
+      "magenta"
+    )(
+      "*---------------------------------------------------------View Departments----------------------------------------------------------*"
+    )
+  );
+
   connection.query(
-    `SELECT 
+    `SELECT
   department.id as 'Dept #ID',
   departmentname as 'Departments'
   from department;`,
@@ -224,8 +272,17 @@ function viewDept() {
   );
 }
 function viewRoles() {
+  console.table(
+    gradient(
+      "cyan",
+      "magenta"
+    )(
+      "*-------------------------------------------------View All Roles--------------------------------------------------------------------*"
+    )
+  );
+
   connection.query(
-    `SELECT 
+    `SELECT
   roles.id as 'Role #ID',
   roles.title as 'Position',
   roles.salary as 'Salary',
@@ -238,35 +295,49 @@ function viewRoles() {
     function (err, data) {
       if (err) throw err;
       console.table(data);
+
       employeesGenerator();
     }
   );
 }
 
 function viewEmployee() {
-  const query = `SELECT
-  employee.id as '#ID',
-  employee.first_name as 'First Name',
-  employee.last_name as 'Last Name',
-  roles.title as 'Position',
-  roles.salary as ' Salary',
-  roles.id as 'Role #ID',
-department.departmentname as 'Departments',
-  employee.manager_id as 'Manager'
-  FROM employee
-  RIGHT JOIN roles
-  ON employee.role_id = roles.id
-Left JOIN department
-  ON roles.department_id= department.id
-  WHERE employee.id IS NOT NULL;`;
+  const query = `SELECT employee.id AS 'ID#', CONCAT(employee.first_name, " ", employee.last_name) AS 'Employees',
+	roles.id AS 'Role ID#', roles.title AS 'Title', roles.salary AS 'Salary',
+    department_id AS 'Dept ID#', department.departmentname  AS 'Department',
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Manager'
+    FROM roles
+    LEFT JOIN employee ON employee.role_id = roles.id
+    INNER JOIN department ON department.id = roles.department_id
+    LEFT JOIN employee e ON employee.manager_id = e.id
+    WHERE employee.id IS NOT NULL;`;
   connection.query(query, function (err, data) {
     if (err) throw err;
+
+    console.table(
+      gradient(
+        "cyan",
+        "magenta"
+      )(
+        "*-------------------------------------------------Table: View All Employees-------------------------------------------------------*"
+      )
+    );
     console.table(data);
-    
+
+
     employeesGenerator();
   });
 }
 function addDept() {
+  console.table(
+    gradient(
+      "cyan",
+      "magenta"
+    )(
+      "*---------------------------------------------------------Add Departments-----------------------------------------------------------*"
+    )
+  );
+
   inquirer
     .prompt([
       {
@@ -286,6 +357,7 @@ function addDept() {
           if (err) {
             console.log(err);
           }
+
           console.table(res);
           employeesGenerator();
         }
@@ -293,72 +365,75 @@ function addDept() {
     });
 }
 function addRoles() {
-  
-      inquirer
-      .prompt([
+  console.table(
+    gradient(
+      "cyan",
+      "magenta"
+    )(
+      "*---------------------------------------------------------Add Roles-----------------------------------------------------------------*"
+    )
+  );
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the job's title?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the employees annual salary",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "Please enter the department ID",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      connection.query(
+        "INSERT INTO roles SET ?",
         {
-          type: "input",
-          name: "title",
-          message: "What is the job's title?",
+          title: response.title,
+          salary: response.salary,
+          department_id: response.department_id,
         },
-        {
-          type: "input",
-          name: "salary",
-          message: "What is the employees annual salary",
-        },
-        {
-          type: "input",
-          name: "department_id",
-          message: "Please enter the department ID",
-        },
-      ])
-      .then((response) => {
-        console.log(response);
-        connection.query(
-          "INSERT INTO roles SET ?",
-          {
-            title: response.title,
-            salary: response.salary,
-            department_id: response.department_id,
-          },
-          function (err, res) {
-            if (err) {
-              console.log(err);
-              console.table(res);
-              employeesGenerator();
-            }
-            connection.query(
-              `SELECT * FROM roles
+        function (err, res) {
+          if (err) {
+            console.log(err);
+            console.table(res);
+            employeesGenerator();
+          }
+          connection.query(
+            `SELECT * FROM roles
       LEFT JOIN department
       ON department.id = department_id
       ORDER BY departmentname;`,
-              (err, data) => {
-                if (err) throw err;
-                console.log(err);
-                console.table(data);
-                employeesGenerator();
-              }
-            );
-          }
-        );
-      });
-};
+            (err, data) => {
+              if (err) throw err;
+              console.log(err);
+              console.table(data);
+              console.table(
+                gradient(
+                  "cyan",
+                  "magenta"
+                )(
+                  "*-----------------------------------------------------------------------------------------------------------------------*"
+                )
+              );
+
+              employeesGenerator();
+            }
+          );
+        }
+      );
+    });
+}
 
 function addEmployee() {
-  const query = `SELECT
-  employee.id as '#ID',
-  employee.first_name as 'First Name',
-  employee.last_name as 'Last Name',
-  roles.title as 'Position',
-  roles.salary as ' Salary',
-  roles.id as 'Role #ID',
-  department.departmentname as 'Departments',
-  employee.manager_id as 'Manager'
-  FROM employee
-  RIGHT JOIN roles
-  ON employee.role_id = roles.id
-Left JOIN department
-  ON roles.department_id= department.id;`;
   inquirer
     .prompt([
       {
@@ -376,22 +451,43 @@ Left JOIN department
         type: "list",
         name: "role_id",
         message: "What is the employee's role's ID?",
-        choices: [5,8,9,10,11,12,13,14,15,17],
+        choices: [5, 8, 9, 10, 11, 12, 13, 14, 15, 17],
       },
 
       {
         type: "list",
         name: "manager_id",
         message: "What is the employee's manager's ID?",
-        choices: [1,2,3,4,5,6,7],
+        choices: [1, 2, 3, 4, 5, 6, 7],
       },
     ])
+
     .then((response) => {
+      const query = `SELECT employee.id AS 'ID#', CONCAT(employee.first_name, " ", employee.last_name) AS 'Employees',
+	roles.id AS 'Role ID#', roles.title AS 'Title', roles.salary AS 'Salary',
+    department_id AS 'Dept ID#', department.departmentname  AS 'Department',
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Manager'
+    FROM roles
+    LEFT JOIN employee ON employee.role_id = roles.id
+    INNER JOIN department ON department.id = roles.department_id
+    LEFT JOIN employee e ON employee.manager_id = e.id
+    WHERE employee.id IS NOT NULL;`;
       connection.query(query, function (err, data) {
         if (err) throw err;
-      console.log(response)
-      console.log(data);
-      },
+
+        console.table(data);
+
+        console.table(
+          gradient(
+            "cyan",
+            "magenta"
+          )(
+            "*-----------------------------------------------Table Add Employee -----------------------------------------------------------*"
+          )
+        );
+
+       
+      });
       connection.query(
         "INSERT INTO employee SET ?",
         {
@@ -405,8 +501,16 @@ Left JOIN department
             console.log(err);
           }
           console.table(data);
-          employeesGenerator();
+          console.log(
+            gradient(
+              "cyan",
+              "magenta"
+            )(
+              "*----------------------------------------------------------------------------------------------------------------------------*"
+            )
+          );
         }
-      ));
+      );
+      employeesGenerator();
     });
-  };
+}
